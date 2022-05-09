9a2f4bc38ce5b2060263029132aec3d33a7112d6
Susanna Carey <susannaopal@gmail.com>
2022-05-09 17:26:06 -0400

    Create file structures for Components and create CSS files for App, Header, and Form

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import '../CSS/Form.css';
+
+const Form = () => {
+  return (
+    <form className = 'form'>
+      <p>Enter prompt</p>
+    </form>
+  )
+}
+
+export default Form;
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import '../CSS/Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>Fun with AI</h1>
+    </div>
+  )
+}
+
+export default Header;
